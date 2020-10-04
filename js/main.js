34486a38ff466be2746f892d64fdfdddb4ca11cc
majahoberg <eaamhob@students.eaaa.dk>
2020-10-04 17:06:57 +0200

    arrow animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_scrollintoview */
-function myFunction() {
+function scrollDown() {
     let elmnt = document.getElementById("content");
     elmnt.scrollIntoView({
         behavior: "smooth" /* makes the scroll smooth */
