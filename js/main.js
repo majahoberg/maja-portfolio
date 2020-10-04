a22e16da15fef10e3adfb5c833dca3127cb8e9b1
majahoberg <eaamhob@students.eaaa.dk>
2020-10-04 16:24:28 +0200

    cleaning css + scroll function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+/* https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_scrollintoview */
+function myFunction() {
+    let elmnt = document.getElementById("content");
+    elmnt.scrollIntoView({
+        behavior: "smooth" /* makes the scroll smooth */
+    });
+  }
+
+
